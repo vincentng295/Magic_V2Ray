0512ddbba8819a4b9ffc0e7d05226053c9cfc024
vincentng295 <151985750+vincentng295@users.noreply.github.com>
2026-06-04 13:33:40 +0700

    Match system apps and multiple user apps

--- service.sh
+++ service.sh
@@ -119,7 +119,7 @@ do_job() {
             # STEP 4: Add iptables rules to mark packets from tun2socks and route them through the tun device
             $iptables -t mangle -N XRAY_MARK
             $iptables -t mangle -A XRAY_MARK -m mark --mark 255 -j RETURN
-            $iptables -t mangle -A XRAY_MARK -m owner --uid-owner 9999-999999 -j MARK --set-xmark 1
+            $iptables -t mangle -A XRAY_MARK -m owner --uid-owner 1000-2147483647 -j MARK --set-xmark 1
             $iptables -t mangle -A OUTPUT -j XRAY_MARK 
             # IPv4 Hotspot support
             $iptables -t mangle -A PREROUTING -i wlan+ -j MARK --set-xmark 1
@@ -142,7 +142,7 @@ do_job() {
             # STEP 4: Add ip6tables rules to mark packets from tun2socks and route them through the tun device
             $ip6tables -t mangle -N XRAY_MARK
             $ip6tables -t mangle -A XRAY_MARK -m mark --mark 255 -j RETURN
-            $ip6tables -t mangle -A XRAY_MARK -m owner --uid-owner 9999-999999 -j MARK --set-xmark 1
+            $ip6tables -t mangle -A XRAY_MARK -m owner --uid-owner 1000-2147483647 -j MARK --set-xmark 1
             $ip6tables -t mangle -A OUTPUT -j XRAY_MARK
             # IPv6 Hotspot support
             $ip6tables -t mangle -A PREROUTING -i wlan+ -j MARK --set-xmark 1
